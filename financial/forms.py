06c626880076c963f9ce500907e97f56b9f85847
Danilka98 <kgbdannn@gmail.com>
2020-12-06 11:14:17 -0500

    Delete function

--- financial/forms.py
+++ financial/forms.py
@@ -1,8 +1,1 @@
 from django import forms
-from .models import Payment
-
-class PaymentCreateForm(forms.ModelForm):
-
-    class Meta:
-        model = Payment
-        fields = ['amount']
--- financial/urls.py
+++ financial/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import (
     PaymentListView,
-    CreatePayment
+    CreatePayment,
+    DeleteView
 )
 from . import views
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contrib_home/', PaymentListView.as_view(), name='contrib-home'),
     #path('contrib_home/', views.contrib_home, name='contrib_home'),
+    path('contrib_home/delete/(?P<pk>[0-9]+)$', DeleteView.as_view(), name='delete-view'),
     path('contrib_home/new/', CreatePayment.as_view(), name='payment-create')
 ]
