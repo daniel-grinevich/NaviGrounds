55b8c5f3faa2176a7f77cba7356304b1e52b3ee2
Danilka98 <kgbdannn@gmail.com>
2020-12-29 11:04:53 -0500

    AWS S3 update

--- financial/models.py
+++ financial/models.py
@@ -14,7 +14,7 @@ class Category(models.Model):
 class Payment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     amount = models.IntegerField(default=0)
-    receipt_img = models.ImageField(upload_to='receipt_pics',null=True)
+    receipt_img = models.ImageField(default='default.jpg', upload_to='receipt_pics')
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     type = models.ManyToManyField(Category)
     time_posted = models.DateTimeField(auto_now_add=True)
--- users/models.py
+++ users/models.py
@@ -11,6 +11,7 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
+"""
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
@@ -20,3 +21,4 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+"""
